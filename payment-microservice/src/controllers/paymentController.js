32f107a8bc8a750d4e9ad255cb29b97b83b95fab
pawan-live <pawan.senpura@gmail.com>
2024-05-15 00:43:17 +0530

    feat: create stripe product endpoint

--- payment-microservice/src/controllers/paymentController.js
+++ payment-microservice/src/controllers/paymentController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import {
   createStripeCheckoutSession,
   handleStripeWebhookEvent,
+  handleCreateProduct,
 } from "../services/stripeService.js";
 
 export const createCheckoutSession = asyncHandler(async (req, res) => {
@@ -25,5 +26,20 @@ export const handleWebhookEvent = asyncHandler(async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Webhook handling error" });
+  }
+});
+
+// controller to create a stripe product using create product service
+// args - name, description, price
+
+export const createProduct = asyncHandler(async (req, res) => {
+  const { name, description, price } = req.body;
+
+  try {
+    const response = await handleCreateProduct(name, description, price);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Failed to create product" });
   }
 });
